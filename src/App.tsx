369be46dc97a12a61155dc1fdb718362718c0236
Mahendra <mahendra.b@intelops.dev>
2023-04-15 13:17:37 +0530

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World
         </a>
       </header>
     </div>
@@ -24,3 +24,4 @@ function App() {
 }
 
 export default App;
+
